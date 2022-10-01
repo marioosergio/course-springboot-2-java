c6b8ea2084151a13f77a50d34767bdc9ad812850
Mario Sergio <msergioc@gmail.com>
2022-09-30 21:42:46 -0300

    Product

---
+++ src/main/java/com/courseproject/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.courseproject.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.courseproject.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
